1da1292e91c65d5f59ffa4a0e7dcb6f6602d7bb0
OlegN5 <6370736@gmail.com>
2021-02-20 17:34:18 +0300

    add statistic

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 //$el = document.querySelector('#editor').textContent
+   
+
+
+
+
+
+
+
+    //console.log(theBigDay)
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBFtqYG8OVlI4E8VdBEMW9mLCBhfeUHHeI",
@@ -14,7 +25,8 @@ const firebaseConfig = {
   var db = firebase.firestore();
 
 
-
+//   document.
+//   class="form-check-input".disabled
 
 let timeStart = 0
 let timeStop = 0
@@ -31,11 +43,31 @@ let hour = 0
 let st = true
 let delay = 1000
 let myTimer
+durOl = 0 
+durSe = 0 
+durKo = 0 
+durSa = 0 
+durMa = 0 
 
 
 let changeN = document.querySelector('#sname')
 changeN.addEventListener('change', function() {
+    sname = document.querySelector('#sname').value
+    document.getElementById('statButton').disabled = false
     document.getElementById('start').disabled = false
+    
+    //debugger
+    if (sname == 1) {
+        myName = 'Олег'
+    } else if (sname == 2) {
+        myName = 'Марк'
+    } else if (sname == 3) {
+        myName = 'Саша'
+    } else if (sname == 4) {
+        myName = 'Костя'
+    } else if (sname == 5) {
+        myName = 'Сергей'
+    }
 
 
 })
@@ -105,14 +137,54 @@ clickStart.addEventListener('click', function() {
     document.getElementById('timeStart').textContent = timeStart
 
 })
+// let checkbox = document.querySelector('#checkDelete');
+
+// checkbox.addEventListener('change', function() {
+//   if (this.checked) {
+//     console.log("Checkbox is checked..");
+//     $el = $el.replace(new RegExp(`[${charsDelete}]`, "g"), "") 
+//     document.querySelector('#result').textContent = $el
+//   } else {
+//     console.log("Checkbox is not checked..");
+//   }
+// });
+
+// let ns = document.getElementById('ns')
+// ns.addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'несинхрон, '
+//         console.log('NS')
+//     }
+//     )
 
 
+//     nr = document.getElementById('nr')
+//     nr.addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'низкая разборчивость, '
+//     }
+//     )
 
+//     document.getElementById('ni').addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'нелинейные искажения, '
+//     }
+//     )
+
+//     it = document.getElementById('it').addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'искажения тембра, '
+//     }
+//     )
+
+//     p = document.getElementById('p').addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'провалы, '
+//     }
+//     )
+
+//     pp = document.getElementById('pp')
+//     pp.addEventListener('change', function() { 
+//         document.getElementById('tBrak').value += 'перепады, '
+//     }
+//     )
 
 let clickStop = document.querySelector('#stop')
-
-
-
 clickStop.addEventListener('dblclick', function() { 
     clearInterval(myTimer)
     document.getElementById('timer').textContent = "Таймер"
@@ -146,17 +218,17 @@ clickStop.addEventListener('dblclick', function() {
     
    
 
-    if (sname === 1) {
-        myName = 'Олег'
-    } else if (sname === 2) {
-        myName = 'Марк'
-    } else if (sname === 3) {
-        myName = 'Саша'
-    } else if (sname === 4) {
-        myName = 'Костя'
-    } else if (sname === 5) {
-        myName = 'Сергей'
-    }
+    // if (sname === 1) {
+    //     myName = 'Олег'
+    // } else if (sname === 2) {
+    //     myName = 'Марк'
+    // } else if (sname === 3) {
+    //     myName = 'Саша'
+    // } else if (sname === 4) {
+    //     myName = 'Костя'
+    // } else if (sname === 5) {
+    //     myName = 'Сергей'
+    // }
 
 
     timeStop = new Date()
@@ -204,7 +276,7 @@ clickStop.addEventListener('dblclick', function() {
      
 
 
-      db.collection("users").add({
+    db.collection("users").add({
         name: myName,
         timeStart: timeStart,
         timeStop: timeStop,
@@ -222,12 +294,110 @@ clickStop.addEventListener('dblclick', function() {
         console.error("Error adding document: ", error);
     });
 
+})
 
-
-
-
-
+let clickStatistic = document.querySelector('#statButton')
+clickStatistic.addEventListener('click', function() { 
     
+  
+    
+    let dataNow = new Date();
+    let smenaStart = new Date();
+    durOl = 0 
+    durSe = 0 
+    durKo = 0 
+    durSa = 0 
+    durMa = 0 
+
+console.log("dataNow.getHour", dataNow.getHours())
+    if (dataNow.getHours()>= 9 && dataNow.getHours() < 21) {
+        //Дневная смена
+       
+        smenaStart.setHours(9);
+        smenaStart.setMinutes(0)   
+    } else if (dataNow.getHours() >= 21) {
+        //Ночная смена
+       
+        smenaStart.setHours(21);
+        smenaStart.setMinutes(0)
+    } else if (dataNow.getHours() < 9) { //<9
+        //!!!!!!!!!!!!!!!ПРОВЕРИТЬ
+        console.log('не туда')
+        
+        smenaStart.setDay((dataNow.getDay())-1)
+        smenaStart.setHours(21);
+        smenaStart.setMinutes(0)
+    }
+console.log("smenaStart", smenaStart)
+//db.collection("test").where("name", "==", myName)
+db.collection("users").where("timeStart", ">", smenaStart)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            //debugger
+            
+            console.log(doc.id, " => ", doc.data());
+            //console.log(document.)
+            my=doc.data()
+            tStart=my.timeStart
+            tStop=my.timeStop
+            duration = tStop.seconds - tStart.seconds
+            console.log("my",my.name)
+            if (my.name==="Саша") {
+                durSa += duration
+            } else if (my.name==="Костя") {
+                durKo += duration
+            } else if (my.name==="Марк") {
+                durMa += duration
+            } else if (my.name==="Сергей") {
+                durSe += duration
+            } else if (my.name==="Олег") {
+                durOl += duration
+            }
+            console.log("my",my.name)
+            console.log("tStart", tStart.seconds)
+            
+
+
+            //my.name
+           
+        });
+    
+let ddate = new Date(0);
+        ddate.setSeconds(durSa);
+        let durSaST = ddate.toISOString().substr(11, 8);
+ddate = new Date(0);
+        ddate.setSeconds(durMa); 
+        let durMaST = ddate.toISOString().substr(11, 8);
+ddate = new Date(0);
+        ddate.setSeconds(durKo); 
+        let durKoST = ddate.toISOString().substr(11, 8);
+ddate = new Date(0);
+        ddate.setSeconds(durOl);
+        let durOlST = ddate.toISOString().substr(11, 8);
+ddate = new Date(0);
+        ddate.setSeconds(durSe);
+        let durSeST = ddate.toISOString().substr(11, 8);
+    
+  
+document.getElementById('statistic').textContent = `ОБНОВЛЕНО: ${new Date()}`
+document.getElementById('sa').textContent = `Саша за смену отработал: ${durSaST}`
+document.getElementById('ma').textContent = `Марк за смену отработал: ${durMaST}`
+document.getElementById('ko').textContent = `Костя за смену отработал: ${durKoST}`
+document.getElementById('ol').textContent = `Олег за смену отработал: ${durOlST}`
+document.getElementById('se').textContent = `Серега за смену отработал: ${durSeST}`
+
+
+ 
+    
+    
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+console.log('statistic')
+
 
 })
 
